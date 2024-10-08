f26ebbec746bca2ab75a318fb4eb52c12097eb53
guilhermeluiiz <luizguilhermedev7@gmail.com>
2024-10-08 18:57:03 -0300

    splash

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
+import React,{useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen'; // Importar a HomeScreen
 import NovaMovimentacao from './screens/NovaMovimentacao'; // Importar a nova tela
 
 const Stack = createNativeStackNavigator();
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  useEffect(() =>{
+    setTimeout(async()=> {
+      await SplashScreen.hideAsync();
+    },3000)
+  },[])
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
